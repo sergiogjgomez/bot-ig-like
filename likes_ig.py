a69f3869b325872cb75b585bf054e1453ba6213b
pharmathyon <sergiogjgomez@gmail.com>
2023-04-20 15:44:28 -0300

    my first commit

---
+++ likes_ig.py
@@ -0,0 +1,47 @@
+import keyboard as kb
+import time
+import pyautogui as auto
+
+bucle = True
+start = False
+tipo = input('Publicacion (p) o historia(h)? (p/h): ')
+
+def on_q_press(event):
+    if event.name == 'q':
+        print('finish')
+        global bucle
+        bucle = False
+
+def on_s_press(event):
+    if event.name == 's':
+        print('start')
+        global start
+        start = True
+
+kb.on_press(on_q_press)
+
+kb.on_press(on_s_press)
+
+bucle_number = 0
+
+while bucle:
+
+    if start:
+    
+        x , y = auto.position()
+        auto.click(x,y)
+        if tipo == 'p':
+            auto.click(x,y)
+        time.sleep(1)
+
+        kb.press_and_release('right')
+        time.sleep(1)
+        
+        bucle_number += 1
+
+        if bucle_number > 1000:
+            break
+
+    else:
+        print('Waiting to start <press "s">, if you leave <press "q">')
+        time.sleep(3)
